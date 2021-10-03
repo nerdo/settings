99db9920bf92b1e963e129cc6d7b59b66a8e3585
Dannel Albert <cuebix@gmail.com>
2021-10-03 15:20:18 -0400

    feat: add wezterm.lua from desktop

---
+++ wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+return {
+  disable_default_key_bindings = true,
+  enable_tab_bar = false,
+  window_background_opacity = 0.9,
+  text_background_opacity = 0.95,
+  font = wezterm.font("Dank Mono"),
+  color_scheme = "Brogrammer",
+}

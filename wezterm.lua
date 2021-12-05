ae87407e485153979e877919be269e69847e49b7
Dannel Albert <cuebix@gmail.com>
2021-12-05 17:53:22 -0500

    fix: fallback fonts to 'Hack Nerd Font Mono'

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,10 @@ return {
   enable_tab_bar = false,
   window_background_opacity = 0.9,
   text_background_opacity = 0.95,
-  font = wezterm.font("Dank Mono"),
+  font = wezterm.font_with_fallback({
+    "Dank Mono",
+    "Hack Nerd Font Mono",
+  }),
   color_scheme = "Brogrammer",
   keys = {
     {key="v", mods="CTRL|SHIFT", action=wezterm.action{PasteFrom="Clipboard"}},

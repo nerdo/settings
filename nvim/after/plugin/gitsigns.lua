baecea5c5a8016aabea82527938600481c5a4607
Dannel Albert <cuebix@gmail.com>
2022-12-20 09:58:25 -0500

    refactor: remove debug log

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,8 +1,6 @@
-print("here")
 require('gitsigns').setup{
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
-  
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr

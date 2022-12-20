a721c543d98eb94590664c9314c5ab098b4a53e0
Dannel Albert <cuebix@gmail.com>
2022-12-20 14:40:26 -0500

    style: remove trailing space

--- nvim/lua/nerdo/auto.lua
+++ nvim/lua/nerdo/auto.lua
@@ -1,8 +1,7 @@
 -- Fix issue with netrw buffers not closing
 -- https://github.com/tpope/vim-vinegar/issues/13#issuecomment-47133890
--- autocmd FileType netrw setl bufhidden=delete
 vim.api.nvim_create_autocmd(
-  "FileType", 
+  "FileType",
   {
     pattern = "netrw",
     command = "setl bufhidden=delete"

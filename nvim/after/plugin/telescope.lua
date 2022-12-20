d4d0ed24dab7690e90738c88b52fc8d660617d72
Dannel Albert <cuebix@gmail.com>
2022-12-20 01:51:48 -0500

    feat: add keybindings for telescope

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,5 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>//', builtin.git_files, {})
+vim.keymap.set('n', '<leader>/f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>/g', builtin.live_grep, {})

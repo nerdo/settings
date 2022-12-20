3d9e20db3a655c7c372c70ca860a87f13098e7a8
Dannel Albert <cuebix@gmail.com>
2022-12-20 11:26:25 -0500

    feat: add lsp-zero

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,103 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  "gopls",
+  "rust_analyzer",
+  "intelephense",
+  "svelte",
+  "tsserver",
+  "html",
+  "cssls",
+  "cssmodules_ls",
+  "tailwindcss",
+  "emmet_ls",
+  "jsonls",
+  "yamlls",
+  "sumneko_lua",
+})
+
+local server_options = {
+	gopls = {
+		settings = {
+			gopls = {
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					compositeLiteralTypes = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					parameterNames = true,
+					rangeVariableTypes = true,
+				},
+			},
+		},
+	},
+
+	tsserver = {
+		settings = {
+			typescript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+			javascript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+		},
+	},
+
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				hint = {
+					enable = true,
+				},
+
+				-- from nvchad's .config/nvim/lua/plugins/configs/lspconfig.lua
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					},
+					maxPreload = 100000,
+					preloadFileSize = 10000,
+				},
+			},
+		},
+	},
+
+	rust_analyzer = {
+		settings = {
+			["rust-analyzer"] = {
+				checkOnSave = {
+					command = "clippy",
+				},
+			},
+		},
+	},
+}
+
+for name, options in pairs(server_options) do
+  lsp.configure(name, options)
+end
+
+lsp.setup()

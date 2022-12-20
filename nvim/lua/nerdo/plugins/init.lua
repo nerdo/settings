02b769bd6a3dce0ab4e5812a63d842a61c86a533
Dannel Albert <cuebix@gmail.com>
2022-12-20 02:11:56 -0500

    feat: add purify color scheme

--- nvim/lua/nerdo/plugins/init.lua
+++ nvim/lua/nerdo/plugins/init.lua
@@ -4,4 +4,11 @@ return function(use)
   -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use {
+    'kyoz/purify', rtp = 'vim',
+    config = function()
+      vim.cmd.colorscheme 'purify'
+    end
+  }
 end

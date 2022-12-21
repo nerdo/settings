584bb49c6595020d90bca9fa515a7309f09f312f
Dannel Albert <cuebix@gmail.com>
2022-12-21 13:53:00 -0500

    feat: add split nav and resize keymaps

--- nvim/lua/nerdo/keymap.lua
+++ nvim/lua/nerdo/keymap.lua
@@ -7,20 +7,32 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
 
 -- Save/close buffer.
-vim.keymap.set("n", "<leader>;", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader><CR>", "<cmd>w<CR>:bd<CR>")
-vim.keymap.set("n", "<leader><BS>", "<cmd>bd<CR>")
+vim.keymap.set("n", "<leader>;", "<Cmd>w<CR>")
+vim.keymap.set("n", "<leader><CR>", "<Cmd>w<CR>:bd<CR>")
+vim.keymap.set("n", "<leader><BS>", "<Cmd>bd<CR>")
 
 -- Save/quit.
-vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>")
-vim.keymap.set("n", "<leader>x", "<cmd>xa<CR>")
+vim.keymap.set("n", "<leader>q", "<Cmd>qa<CR>")
+vim.keymap.set("n", "<leader>x", "<Cmd>xa<CR>")
 
 -- Tab through buffers.
-vim.keymap.set("n", "<Tab>", "<cmd>bn<CR>")
-vim.keymap.set("n", "<S-Tab>", "<cmd>bp<CR>")
+vim.keymap.set("n", "<Tab>", "<Cmd>bn<CR>")
+vim.keymap.set("n", "<S-Tab>", "<Cmd>bp<CR>")
 
 -- Path functions.
-vim.keymap.set("n", "<leader>yr", "<cmd>CpRelPath<CR>")
-vim.keymap.set("n", "<leader>ya", "<cmd>CpAbsPath<CR>")
-vim.keymap.set("n", "<leader>R", "<cmd>ShowRelPath<CR>")
-vim.keymap.set("n", "<leader>A", "<cmd>ShowAbsPath<CR>")
+vim.keymap.set("n", "<leader>yr", "<Cmd>CpRelPath<CR>")
+vim.keymap.set("n", "<leader>ya", "<Cmd>CpAbsPath<CR>")
+vim.keymap.set("n", "<leader>R", "<Cmd>ShowRelPath<CR>")
+vim.keymap.set("n", "<leader>A", "<Cmd>ShowAbsPath<CR>")
+
+-- Split navigation with Alt+nav keys.
+vim.keymap.set("n", "<A-h>", "<Cmd>wincmd h<CR>")
+vim.keymap.set("n", "<A-j>", "<Cmd>wincmd j<CR>")
+vim.keymap.set("n", "<A-k>", "<Cmd>wincmd k<CR>")
+vim.keymap.set("n", "<A-l>", "<Cmd>wincmd l<CR>")
+
+-- Split resizing with Alt+Shift+nav keys.
+vim.keymap.set("n", "<A-S-h>", "<Cmd>vertical resize -1<CR>")
+vim.keymap.set("n", "<A-S-j>", "<Cmd>resize +1<CR>")
+vim.keymap.set("n", "<A-S-k>", "<Cmd>resize -1<CR>")
+vim.keymap.set("n", "<A-S-l>", "<Cmd>vertical resize +1<CR>")

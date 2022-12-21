a163ccd441434b039403b8f99009d2c3d3fb1162
Dannel Albert <cuebix@gmail.com>
2022-12-21 13:52:39 -0500

    feat: add keymap, double esc to go exit terminal mode

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -5,3 +5,12 @@ if not present then
 end
 
 toggleterm.setup()
+
+-- Set up some keybindings just for the terminal buffer.
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*",
+	callback = function()
+		local opts = { buffer = 0 }
+		vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], opts)
+	end,
+})

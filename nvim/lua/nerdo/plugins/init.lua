efa337eaeac5fda1daa60bad780c06eebf836f8a
Dannel Albert <cuebix@gmail.com>
2022-12-21 08:09:06 -0500

    feat: add nvim-colorizer

--- nvim/lua/nerdo/plugins/init.lua
+++ nvim/lua/nerdo/plugins/init.lua
@@ -1,66 +1,69 @@
 return function(use)
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  use { 'kyoz/purify', rtp = 'vim', }
+	use({ "kyoz/purify", rtp = "vim" })
 
-  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+	use({ "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } })
 
-  use { 'nvim-treesitter/nvim-treesitter-context', { after = 'nvim-treesitter' } }
+	use({ "nvim-treesitter/nvim-treesitter-context", { after = "nvim-treesitter" } })
 
-  use { 'nvim-treesitter/playground', { after = 'nvim-treesitter', run = ':TSInstall query' } }
+	use({ "nvim-treesitter/playground", { after = "nvim-treesitter", run = ":TSInstall query" } })
 
-  use 'lewis6991/gitsigns.nvim'
+	use("lewis6991/gitsigns.nvim")
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
-  }
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-  -- Clear search highlighting when done.
-  use 'romainl/vim-cool'
+	-- Clear search highlighting when done.
+	use("romainl/vim-cool")
 
-  -- Align text with spaces easily.
-  use 'junegunn/vim-easy-align'
+	-- Align text with spaces easily.
+	use("junegunn/vim-easy-align")
 
-  -- Highlights unique characters to jump to in a line.
-  use 'unblevable/quick-scope'
+	-- Highlights unique characters to jump to in a line.
+	use("unblevable/quick-scope")
 
-  -- Automatic detection of tab size.
-  use 'Darazaki/indent-o-matic'
+	-- Automatic detection of tab size.
+	use("Darazaki/indent-o-matic")
 
-  -- Inlay hints for type/metadata.
-  use 'lvimuser/lsp-inlayhints.nvim'
+	-- Inlay hints for type/metadata.
+	use("lvimuser/lsp-inlayhints.nvim")
 
-  -- Show indent lines.
-  use "lukas-reineke/indent-blankline.nvim"
+	-- Show indent lines.
+	use("lukas-reineke/indent-blankline.nvim")
 
-  -- Automatically close html/jsx tags.
-  use "windwp/nvim-ts-autotag"
+	-- Automatically close html/jsx tags.
+	use("windwp/nvim-ts-autotag")
 
-  -- Comment out lines with keymap.
-  use 'numToStr/Comment.nvim'
+	-- Comment out lines with keymap.
+	use("numToStr/Comment.nvim")
 
-  -- For formatting code.
-  use "jose-elias-alvarez/null-ls.nvim"
+	-- For formatting code.
+	use("jose-elias-alvarez/null-ls.nvim")
+
+	-- Colorizer for displaying colors in code.
+	use("norcalli/nvim-colorizer.lua")
 end

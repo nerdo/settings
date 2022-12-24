c8b8dd8f8fc7bdce114e519e1c1a3906f6379f2b
Dannel Albert <cuebix@gmail.com>
2022-12-24 18:11:58 -0500

    feat: setup basic dap, rust settings

---
+++ nvim/after/plugin/dap.lua
@@ -0,0 +1,34 @@
+local present, dap = pcall(require, "dap")
+
+if not present then
+	return
+end
+
+-- Set up debugging keymaps.
+vim.keymap.set("n", "<F5>", function()
+	dap.continue()
+end)
+vim.keymap.set("n", "<F8>", function()
+	dap.step_out()
+end)
+vim.keymap.set("n", "<F9>", function()
+	dap.step_into()
+end)
+vim.keymap.set("n", "<F10>", function()
+	dap.step_over()
+end)
+vim.keymap.set("n", "<leader>db", function()
+	dap.toggle_breakpoint()
+end)
+vim.keymap.set("n", "<leader>dB", function()
+	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end)
+vim.keymap.set("n", "<leader>dl", function()
+	dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end)
+vim.keymap.set("n", "<leader>dr", function()
+	dap.repl.open()
+end)
+vim.keymap.set("n", "<leader>dR", function()
+	dap.run_last()
+end)

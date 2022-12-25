a4d36f3f2259038f93de07bc0c23c0a38ba14a3e
Dannel Albert <cuebix@gmail.com>
2022-12-24 21:43:47 -0500

    feat(nvim): clearer bufferline active buffer indicator

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -4,4 +4,21 @@ if not present then
 	return
 end
 
-bufferline.setup({})
+-- You can change the appearance of the bufferline separators by setting the
+-- `separator_style`. The available options are:
+-- * `slant` - Use slanted/triangular separators
+-- * `padded_slant` - Same as `slant` but with extra padding which some terminals require.
+--   If `slant` does not render correctly for you try padded this instead.
+-- * `thick` - Increase the thickness of the separator characters
+-- * `thin` - (default) Use thin separator characters
+-- * finally you can pass in a custom list containing 2 characters which will be
+--   used as the separators e.g. `{"|", "|"}`, the first is the left and the
+--   second is the right separator
+bufferline.setup({
+	options = {
+		indicator = {
+			icon = "▌",
+		},
+		separator_style = { " ", " " },
+	},
+})

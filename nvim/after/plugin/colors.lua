2613ff2b9b58aa5b424b06345b6cd221f7323a5b
Dannel Albert <cuebix@gmail.com>
2022-12-27 14:16:18 -0500

    feat(nvim): customize telescope colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -13,7 +13,7 @@ vim.api.nvim_set_hl(0, "LineNrAbove", { ctermfg = "red", fg = "#C46EA2" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { ctermfg = "green", fg = "#6EC48B" })
 
 -- Set visual highlight.
-vim.api.nvim_set_hl(0, "Visual", { ctermbg = "Grey", bg = "#044A4E" })
+vim.api.nvim_set_hl(0, "Visual", { ctermbg = "Grey", bg = "#0b3b3d" })
 
 -- Set highlight group for importing TS types for a nice visual distinction.
 vim.api.nvim_set_hl(0, "TSConstructor", { fg = "#25888E" })
@@ -21,3 +21,12 @@ vim.api.nvim_set_hl(0, "TSConstructor", { fg = "#25888E" })
 -- Set highlights for quick scope (highlights letters for quick horizontal movement).
 vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#afff5f", bold = true, ctermfg = 155 })
 vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5fffff", bold = true, ctermfg = 81 })
+
+-- Telescope.
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#2d3033" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "#292c33" })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#2d3033" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "#434854" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#2d3033", bg = "#2d3033" })
+vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = "#fff36d" })

53f40fa3ca9160f6a260f42a172f8e14b7506050
Dannel Albert <cuebix@gmail.com>
2022-12-27 14:19:30 -0500

    feat(nvim): add keymap for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -11,6 +11,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-g>", builtin.git_files, {})
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>?", "<Cmd>Telescope<CR>")
 
 if pcall(require, "telescope._extensions.ui-select") then
 	telescope.load_extension("ui-select")

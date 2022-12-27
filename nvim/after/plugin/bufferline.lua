4aa0d41617621524f73bdc5b111c72cf340cc24c
Dannel Albert <cuebix@gmail.com>
2022-12-27 13:27:57 -0500

    feat(nvim): emphasize selected buffers

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -6,9 +6,41 @@ end
 
 bufferline.setup({
 	options = {
+		-- modified_icon = "✏️",
 		indicator = {
-			icon = "▐▏",
+			-- It kind of resembles a chat bubble... I like it !
+			icon = "◢█",
 		},
 		separator_style = { " ", " " },
+	},
+
+	-- :help bufferline-highlights
+	highlights = {
+		indicator_selected = {
+			fg = "#F2E9EA",
+		},
+		buffer_selected = {
+			bg = "#F2E9EA",
+			fg = "#393d47",
+			bold = true,
+		},
+		-- i.e. When there are duplicate file names open in different paths,
+		-- this highlight is for the path part...
+		duplicate_selected = {
+			bg = "#F2E9EA",
+			bold = false,
+		},
+		close_button_selected = {
+			bg = "#F2E9EA",
+			fg = "#b30018",
+			bold = true,
+		},
+		modified = {
+			fg = "#fe5000",
+		},
+		modified_selected = {
+			bg = "#F2E9EA",
+			fg = "#fe5000",
+		},
 	},
 })

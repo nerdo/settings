c0d8ee5dd07acbb441f8961cd88e8133f61a9180
Dannel Albert <cuebix@gmail.com>
2022-12-27 11:00:46 -0500

    feat(nvim): add keymap to open trouble and keep current buffer active
    it's written based on the assumption that opening trouble happens SYNCHRONOUSLY, so it may break in the future...

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -16,3 +16,18 @@ vim.keymap.set("n", "<leader>id", "<cmd>TroubleToggle document_diagnostics<cr>",
 vim.keymap.set("n", "<leader>il", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>iq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+
+local nerdo = require("nerdo.functions")
+
+-- This keymap runs a function that toggles Trouble open but retains focus on the current buffer (by going back to the previous buffer immediately).
+vim.keymap.set("n", "<leader>io", function()
+	local trouble_was_open = nerdo.editor.buffer_filetype_is_open("Trouble")
+
+	-- Assuming opening Trouble happens synchronously...
+	vim.cmd("TroubleToggle")
+
+	-- This should work, assuming the above holds true...
+	if not trouble_was_open then
+		vim.cmd("bp")
+	end
+end)

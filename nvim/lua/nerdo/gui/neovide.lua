c391f24f9c9c7935cad222717e9eecac12956f57
Dannel Albert <cuebix@gmail.com>
2022-12-31 14:43:12 -0500

    fix(nvim): unhide mouse cursor in neovide

--- nvim/lua/nerdo/gui/neovide.lua
+++ nvim/lua/nerdo/gui/neovide.lua
@@ -7,7 +7,7 @@ vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_floating_opacity = 0.9
-vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_hide_mouse_when_typing = false
 
 require("nerdo.gui.neovide-font-sizing")
 

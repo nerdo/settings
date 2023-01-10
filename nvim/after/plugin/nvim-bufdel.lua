9967e372ca3b1e08e5455c70568174720fb0b93c
Dannel Albert <cuebix@gmail.com>
2023-01-10 01:03:50 -0500

    fix(nvim): workaround trouble bug where closing buffer with it open exits vim

--- nvim/after/plugin/nvim-bufdel.lua
+++ nvim/after/plugin/nvim-bufdel.lua
@@ -12,10 +12,31 @@ local nerdo = require("nerdo.functions")
 
 -- Close splits, or when there is only one, close buffers.
 vim.keymap.set("n", "<leader><BS>", function()
+	-- Workaround for vim quitting when closing a buffer and Trouble is open.
+	local trouble_was_open = nerdo.editor.buffer_filetype_is_open("Trouble")
+	if trouble_was_open then
+		vim.cmd("TroubleToggle")
+	end
+
 	nerdo.editor.win_or_buf("close", "BufDel")
+
+	if trouble_was_open then
+		vim.cmd("TroubleToggle")
+		vim.cmd("bp")
+	end
 end)
 
 -- Just close the buffer.
 vim.keymap.set("n", "\\<BS>", function()
+	local trouble_was_open = nerdo.editor.buffer_filetype_is_open("Trouble")
+	if trouble_was_open then
+		vim.cmd("TroubleToggle")
+	end
+
 	nerdo.editor.win_or_buf("BufDel", "BufDel")
+
+	if trouble_was_open then
+		vim.cmd("TroubleToggle")
+		vim.cmd("bp")
+	end
 end)

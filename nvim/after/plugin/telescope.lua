46af86e809df1fa0475d2c8c49212156c11b98dc
Dannel Albert <cuebix@gmail.com>
2023-01-11 23:44:30 -0500

    feat(nvim): telescope keybindings in normal, insert mode

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -8,9 +8,9 @@ telescope.setup()
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<C-g>", builtin.git_files, {})
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+vim.keymap.set({ "n", "i" }, "<C-g>", builtin.git_files, {})
+vim.keymap.set({ "n", "i" }, "<C-p>", builtin.find_files, {})
+vim.keymap.set({ "n", "i" }, "<C-f>", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>?", "<Cmd>Telescope<CR>")
 
 if pcall(require, "telescope._extensions.ui-select") then

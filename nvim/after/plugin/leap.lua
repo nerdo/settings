4627c6aba03ed1617988352dc6c02df10514095f
Dannel Albert <cuebix@gmail.com>
2023-02-02 11:10:55 -0500

    feat(nvim): fix deleting with visual selection

--- nvim/after/plugin/leap.lua
+++ nvim/after/plugin/leap.lua
@@ -4,4 +4,8 @@ if not present then
 	return
 end
 
+-- Leap caniballizes these two keymaps, which mess with standard vim behavior,
+-- but if we define a keymap, it won't overwrite it!
+vim.keymap.set({ "x", "o" }, "x", "x", { noremap = true })
+
 leap.add_default_mappings()

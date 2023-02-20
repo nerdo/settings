025a22c6b8044544844575c810870fec50ca1fec
Dannel Albert <cuebix@gmail.com>
2023-02-19 21:37:59 -0500

    fix(nvim): add lspsaga dependencies for treesitter
    apparently it needs markdown, markdown_inline for some odd reason?

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -6,15 +6,25 @@ end
 
 configs.setup({
 	-- A list of parser names, or "all"
-	ensure_installed = { "help", "javascript", "typescript", "go", "json", "css", "yaml", "c", "lua", "rust" },
-
+	ensure_installed = {
+		"help",
+		"javascript",
+		"typescript",
+		"go",
+		"json",
+		"css",
+		"yaml",
+		"c",
+		"lua",
+		"rust",
+		"markdown",
+		"markdown_inline",
+	},
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
-
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 	auto_install = true,
-
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 

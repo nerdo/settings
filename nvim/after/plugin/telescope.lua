ba41ad3ddcffa45d4a082f4b04951bcc60b9f961
Dannel Albert <cuebix@gmail.com>
2023-04-04 23:07:03 -0400

    fix: swap git/find files, add meta-f for grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -14,9 +14,10 @@ telescope.setup({
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set({ "n", "i" }, "<C-p>", builtin.git_files, {})
-vim.keymap.set({ "n", "i" }, "<A-p>", builtin.find_files, {})
+vim.keymap.set({ "n", "i" }, "<A-p>", builtin.git_files, {})
+vim.keymap.set({ "n", "i" }, "<C-p>", builtin.find_files, {})
 vim.keymap.set({ "n", "i" }, "<C-f>", builtin.live_grep, {})
+vim.keymap.set({ "n", "i" }, "<A-f>", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>?", "<Cmd>Telescope<CR>")
 
 if pcall(require, "telescope._extensions.ui-select") then

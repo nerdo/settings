a5cb95d1901dc7d187d1752c0fa7031bdc29ba3e
Dannel Albert <cuebix@gmail.com>
2023-05-23 17:16:48 -0400

    fix(nvim): update inlayhints on_attach param

--- nvim/after/plugin/lsp-inlayhints.lua
+++ nvim/after/plugin/lsp-inlayhints.lua
@@ -30,7 +30,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		local bufnr = args.buf
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		inlayhints.on_attach(client, bufnr)
+		inlayhints.on_attach(client, bufnr, false)
 	end,
 })
 

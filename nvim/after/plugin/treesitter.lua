032ea869b6d72086d4487e5515fda15ea28f1a97
Dannel Albert <cuebix@gmail.com>
2023-07-13 13:28:54 -0400

    fix(nvim): disable treesitter on large files

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,4 @@
 local present, configs = pcall(require, "nvim-treesitter.configs")
-local nerdo = require("nerdo.functions")
 
 if not present or vim.g.vscode then
 	return
@@ -21,17 +20,36 @@ configs.setup({
 		"markdown",
 		"markdown_inline",
 	},
+
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
+
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 	auto_install = true,
+
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
 	highlight = {
 		-- `false` will disable the whole extension
 		enable = true,
+
+		-- Disable on large files.
+		disable = function(_lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			local s = ""
+			if ok and stats and stats.size then
+				s = stats.size
+			end
+
+			print("treesitter filesize detected = " .. s .. ", max = " .. max_filesize)
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages

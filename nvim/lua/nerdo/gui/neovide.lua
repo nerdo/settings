a70164babd7e4aadff859b3f1df8e3fd3f43ced1
Dannel Albert <cuebix@gmail.com>
2023-08-02 15:22:21 -0400

    fix(nvim): set opaque bg
    Transparency has some issues. This is cleaner, more consistent.

--- nvim/lua/nerdo/gui/neovide.lua
+++ nvim/lua/nerdo/gui/neovide.lua
@@ -2,7 +2,7 @@ if vim.g.neovide == false then
 	return
 end
 
-vim.g.neovide_transparency = 0.85
+vim.g.neovide_transparency = 1.0
 vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_cursor_vfx_mode = "ripple"

c86b472f25a3e0b3b9481cc521d2657acb52417c
Dannel Albert <cuebix@gmail.com>
2023-10-20 00:40:46 -0400

    fix(nvim): correct setup of nvim-autopairs

--- nvim/lua/nerdo/plugins/nvim-autopairs.lua
+++ nvim/lua/nerdo/plugins/nvim-autopairs.lua
@@ -1,7 +1,9 @@
 -- Auto pairing of brackets and such.
 return {
 	"windwp/nvim-autopairs",
+	event = "InsertEnter",
 	enabled = function()
 		return not vim.g.vscode
 	end,
+	opts = {}, -- equivalent to calling setup({}) function
 }

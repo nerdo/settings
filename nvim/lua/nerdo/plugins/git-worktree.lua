d7f05319759a334bc2ee60cf026402f815d21a3b
Dannel Albert <cuebix@gmail.com>
2023-10-20 11:46:57 -0400

    feat(nvim): add git-worktree plugin

---
+++ nvim/lua/nerdo/plugins/git-worktree.lua
@@ -0,0 +1,19 @@
+-- Navigate git worktrees. Thanks ThePrimeagen :D.
+return {
+	"ThePrimeagen/git-worktree.nvim",
+	config = function()
+		local gitworktree = require("git-worktree")
+		gitworktree.setup({})
+
+		local telescope_is_present, telescope = pcall(require, "telescope")
+		if telescope_is_present then
+			telescope.load_extension("git_worktree")
+			vim.keymap.set({ "n" }, "<leader>wc", function()
+				telescope.extensions.git_worktree.create_git_worktree()
+			end)
+			vim.keymap.set({ "n" }, "<leader>wo", function()
+				telescope.extensions.git_worktree.git_worktrees()
+			end)
+		end
+	end,
+}

e873a559bd5b1d50f22cf4f6ccd6838707410a74
Dannel Albert <cuebix@gmail.com>
2024-01-25 14:16:57 -0500

    feat: tweak neotest settings

--- nvim/lua/nerdo/plugins/neotest.lua
+++ nvim/lua/nerdo/plugins/neotest.lua
@@ -31,22 +31,34 @@ return {
 			},
 		})
 
+		local runSingleTest = function()
+			-- Run single test.
+			neotest.run.run()
+		end
+		local runFileTests = function()
+			-- Run entire file.
+			neotest.run.run(vim.fn.expand("%"))
+		end
+		local runLastTest = function()
+			neotest.run.run_last()
+		end
+
 		vim.keymap.set("n", "<leader>th", function()
 			neotest.summary.toggle()
 		end)
-		vim.keymap.set({ "n", "i" }, "<M-t>", function()
-			-- Run single test.
-			neotest.run.run()
-		end)
-		vim.keymap.set({ "n", "i" }, "<C-t>", function()
-			-- Run entire file.
-			neotest.run.run(vim.fn.expand("%"))
-		end)
+
+		vim.keymap.set({ "n", "i" }, "<M-t>", runSingleTest)
+		vim.keymap.set({ "n", "i" }, "<C-t>", runFileTests)
+
+		vim.keymap.set({ "n", "i" }, "<leader>tr", runSingleTest)
+		vim.keymap.set({ "n", "i" }, "<leader>tt", runFileTests)
+		vim.keymap.set({ "n", "i" }, "<leader>tl", runLastTest)
+
 		vim.keymap.set("n", "<leader>dt", function()
 			neotest.run.run({ strategy = "dap" })
 		end)
 		vim.keymap.set("n", "<leader>to", function()
-			neotest.output.open()
+			neotest.output.open({ enter = true })
 		end)
 		vim.keymap.set("n", "<leader>tO", function()
 			neotest.output_panel.toggle()

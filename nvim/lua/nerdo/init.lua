4f568a2e127b7f582999d9610e97d6bd8130f7f5
Dannel Albert <cuebix@gmail.com>
2025-01-24 15:26:51 -0500

    fix: only load keymaps when running in vscode

--- nvim/lua/nerdo/init.lua
+++ nvim/lua/nerdo/init.lua
@@ -1,7 +1,13 @@
-require("nerdo.functions")
-require("nerdo.keymap")
-require("nerdo.options")
-require("nerdo.lazy")
-require("nerdo.gui")
-require("nerdo.workspace")
-require("nerdo.persistent-undo")
+if vim.g.vscode then
+	-- Only load keymaps when running vscode.
+	-- Plugins and other stuff get in the way of it running properly.
+	require("nerdo.keymap")
+else
+	require("nerdo.functions")
+	require("nerdo.keymap")
+	require("nerdo.options")
+	require("nerdo.lazy")
+	require("nerdo.gui")
+	require("nerdo.workspace")
+	require("nerdo.persistent-undo")
+end
